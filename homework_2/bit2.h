e24b34b1d853d1b811dd70ba8bad774bc3ab8a85
Deanna <deanna.bessy@tufts.edu>
2015-11-04 15:45:27 -0500

    added design docs. see these for explanation of work

--- homework_2/bit2.h
+++ homework_2/bit2.h
@@ -14,60 +14,40 @@
 #define T Bit2_T
 typedef struct T *T; 
 
-/* 
- * Creates a 2D bitmap and sets all values to 0. Bit2_new can raise Mem_failed.
- * It is a checked runtime error to pass negative values for col or row.
- */
+/* Creates a 2D bitmap and sets all values to 0. */
 extern Bit2_T Bit2_new(int col, int row);
 
-/* 
- * Returns the number of cols in the bitmap. It is a checked runtime error to 
- * pass a NULL value for bit2.
- */
+/* Returns the number of cols in the bitmap.  */
 extern int Bit2_width(Bit2_T bit2);
 
-/* 
- * Returns the number of rows in the bitmap. It is a checked runtime error to 
- * pass a NULL value for bit2.
- */ 
+/* Returns the number of rows in the bitmap. */ 
 extern int Bit2_height(Bit2_T bit2); 
 
-/* 
- * Returns the number of 1s in the bitmap. It is a checked runtime error to 
- * pass a NULL value for bit2.
- */
+/* Returns the number of 1s in the bitmap. */
 extern int Bit2_count(Bit2_T bit2);
 
-/* 
- * Frees memory associated with bitmap. It is a checked runtime error to 
- * pass a NULL value for bit2.
- */
+/* Frees memory associated with bitmap. */
 extern void Bit2_free(Bit2_T *bit2);
 
-/* Returns the bit at specified col and row. It is a checked runtime error to 
- * pass a negative value for col or row.
- */
+/* Returns the bit at specified col and row. */
 extern int Bit2_get(Bit2_T bit2, int col, int row); 
 
-/* 
- * Sets the bit at specified col and row to specified value (either 0 or 1).  
+/* Sets the bit at specified col and row to specified value (either 0 or 1).  
  * Returns previous value of specified bit.
  */
 extern int Bit2_put(Bit2_T bit2, int col, int row, int bit);
 
-/* 
- * Calls apply on each bit in the bitmap with row indeces changing more rapidly
+/* Calls apply on each bit in the bitmap with row indeces changing more rapidly
  * than col indeces. 
  */ 
 extern void Bit2_map_col_major(Bit2_T bit2, void apply(int col, int row,
-                               Bit2_T bit2, int bit, void *cl), void *cl);
+                               Bit2_T bit2, int b, void *c1), void *c1);
 
-/* 
- * Calls apply on each bit in the bitmap with col indeces changing more rapidly
- * than row indeces. 
+/* Calls apply on each bit in the bitmap with col indeces changing more rapidly
+ * than row indeces 
  */
 extern void Bit2_map_row_major(Bit2_T bit2, void apply(int col, int row,
-                               Bit2_T bit2, int bit, void *cl), void *cl);
+                               Bit2_T bit2, int b, void *c1), void *c1);
 
 #undef T
 #endif
